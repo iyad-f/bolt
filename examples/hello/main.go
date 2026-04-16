1f95a355d2f9a0048cb34df32828d51f56382dce
Iyad Farooq Zargar <128908811+iyad-f@users.noreply.github.com>
2026-04-16 22:08:36 +0530

    chore: update example to echo method, path, and body

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/iyad-f/bolt"
@@ -12,8 +13,13 @@ func main() {
 		Addr: ":8080",
 		Handler: bolt.HandlerFunc(func(w bolt.ResponseWriter, r *bolt.Request) {
 			w.Header().Set("Content-Type", "text/plain")
-			w.WriteHeader(bolt.StatusOK)
-			w.Write([]byte("Hello, World!"))
+
+			body, _ := io.ReadAll(r.Body)
+			if len(body) > 0 {
+				fmt.Fprintf(w, "%s %s\nBody: %s", r.Method, r.RequestURI, string(body))
+			} else {
+				fmt.Fprintf(w, "%s %s\nHello, World!", r.Method, r.RequestURI)
+			}
 		}),
 	}
 
