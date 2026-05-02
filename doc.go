6ae5558b3fb9a98435633a84c48db7d74bdd4313
iyad-f <128908811+iyad-f@users.noreply.github.com>
2026-05-02 22:43:04 +0530

    docs: add package documentation

---
+++ doc.go
@@ -0,0 +1,16 @@
+// Copyright 2026 Iyad
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package bolt is a lightweight HTTP/1.1 server built from raw TCP sockets.
+package bolt
